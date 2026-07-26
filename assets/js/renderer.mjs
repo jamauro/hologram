4d812f12dfae9ee7881f317166d91e2bafbc221a
jamauro <jamauro@gmail.com>
2026-07-26 14:53:11 -0500

    Revert "Expose fast-path hit counters and a switch to disable it"
    This reverts commit 03b7e7b82890c15c92ccd24109e26eb879b594ea.

--- assets/js/renderer.mjs
+++ assets/js/renderer.mjs
@@ -781,11 +781,9 @@ export default class Renderer {
       Type.nil(),
     );
 
-    if (Type.isNil(loopKey) || globalThis.Hologram?.disableFastPath === true) {
+    if (Type.isNil(loopKey)) {
       return null;
     }
-
-    Renderer.fastPathAttempts++;
 
     const enclosing = Renderer.#memoStack.at(-1);
 
@@ -827,7 +825,6 @@ export default class Renderer {
       return null;
     }
 
-    Renderer.fastPathHits++;
     enclosing.children.add(cidKey);
     Renderer.#renderParents.set(cidKey, scopeKey);
 
@@ -841,10 +838,6 @@ export default class Renderer {
   }
 
   static #keyedCids = new Map();
-
-  // Diagnostics for the fast path (see #cachedKeyedComponent), read from the console.
-  static fastPathAttempts = 0;
-  static fastPathHits = 0;
 
   static #castProps(propsDom, moduleProxy) {
     const propsTuples = Renderer.#filterAllowedProps(propsDom, moduleProxy)
